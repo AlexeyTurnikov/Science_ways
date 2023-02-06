3c6b4104fb4549de532b66376cfa47815c24f19f
alexe <tiurnikov.aiu@phystech.edu>
2023-02-06 17:53:14 +0300

    Good 3D mesh

--- Lab1/Lab1.1.py
+++ Lab1/Lab1.1.py
@@ -5,13 +5,13 @@ import numpy as np
 gmsh.initialize()
 gmsh.model.add("1.1")
 
-lc = 0.05
+lc = 0.1
 points_tags_global = []
 points_coord_global = []
 
 
 def find_points(point_coord, point_coord_new):
-    for i in range(0, len(point_coord)):
+    for i in range(0, len(point_coord), 3):
         if point_coord[i:i + len(point_coord_new)] == point_coord_new:
             return True, i
     return False, 0
@@ -19,6 +19,9 @@ def find_points(point_coord, point_coord_new):
 
 def create_point(x, y, z):
     global points_coord_global
+    x = round(x, 3)
+    y = round(y, 3)
+    z = round(z, 3)
     tag = gmsh.model.geo.addPoint(x, y, z, lc)
     what_happened, j = find_points(points_coord_global, [x, y, z])
     if not what_happened:
@@ -30,10 +33,9 @@ def create_point(x, y, z):
 
 
 def arc(rad, phi, z):
-    point_tags = []
-    point_tags.append(create_point(0, 0, z))
-    point_tags.append(create_point(rad * np.cos(phi), rad * np.sin(phi), z))
-    point_tags.append(create_point(rad * np.cos(phi + np.pi / 2), rad * np.sin(phi + np.pi / 2), z))
+    point_tags = [create_point(0, 0, z),
+                  create_point(rad * np.cos(phi), rad * np.sin(phi), z),
+                  create_point(rad * np.cos(phi + np.pi / 2), rad * np.sin(phi + np.pi / 2), z)]
     return gmsh.model.geo.addCircleArc(point_tags[1], point_tags[0], point_tags[2])
 
 
@@ -81,20 +83,12 @@ def part_torus(rad1, rad2, phi, z0):
     curve_part.append(gmsh.model.geo.addCurveLoop([curve_part[1], curve_part[6], -curve_part[2], -curve_part[5]]))
     curve_part.append(gmsh.model.geo.addCurveLoop([curve_part[0], curve_part[9], -curve_part[7], -curve_part[8]]))
     curve_part.append(gmsh.model.geo.addCurveLoop([curve_part[7], curve_part[11], -curve_part[2], -curve_part[10]]))
+
     s1 = gmsh.model.geo.addSurfaceFilling([curve_part[-4]])
     s2 = gmsh.model.geo.addSurfaceFilling([curve_part[-3]])
     s3 = gmsh.model.geo.addSurfaceFilling([curve_part[-2]])
     s4 = gmsh.model.geo.addSurfaceFilling([curve_part[-1]])
-    # Точки для боковой поверхности
-    p1 = create_point(rad1 * np.cos(phi), rad1 * np.sin(phi), rad2 * 2 + z0)
-    p2 = create_point((rad1 + rad2) * np.cos(phi), (rad1 + rad2) * np.sin(phi), rad2 + z0)
-    p3 = create_point((rad1 - rad2) * np.cos(phi), (rad1 - rad2) * np.sin(phi), rad2 + z0)
-    p4 = create_point(rad1 * np.cos(phi), rad1 * np.sin(phi), z0)
-    c1 = curve_part[3]
-    c2 = curve_part[5]
-    c3 = curve_part[8]
-    c4 = curve_part[10]
-    return [s1, s2, s3, s4, p1, p2, p3, p4, c1, c2, c3, c4]
+    return [s1, s2, s3, s4]
     # наружная верхняя, наружная нижняя, внутренняя верхняя, внутренняя нижняя
 
 
@@ -115,50 +109,12 @@ surfaces += part_torus(radius1, radius22, np.pi / 2, radius21 - radius22)
 surfaces += part_torus(radius1, radius22, np.pi, radius21 - radius22)
 surfaces += part_torus(radius1, radius22, 1.5 * np.pi, radius21 - radius22)
 
-# Прямые для боковых поверхностей
-lines = []
-for i in range(0, len(surfaces) // 2, 12):
-    i += 4
-    lines.append(gmsh.model.geo.addLine(surfaces[i + 1], surfaces[i + 49]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i], surfaces[i + 48]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i + 3], surfaces[i + 51]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i + 1], surfaces[i + 49]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i + 2], surfaces[i + 50]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i], surfaces[i + 48]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i + 3], surfaces[i + 51]))
-    lines.append(gmsh.model.geo.addLine(surfaces[i + 2], surfaces[i + 50]))
+sl1 = gmsh.model.geo.addSurfaceLoop(surfaces)
+gmsh.model.geo.addVolume([sl1])
 
-for k in range(4):
-    del surfaces[4:len(surfaces):12 - k]
-
-curves = []
-
-for i in range(0, len(surfaces) // 2, 8):
-    j = i
-    for k in range(0, 4):
-        cl = gmsh.model.geo.addCurveLoop([surfaces[i + 4], lines[j + 2 * k], -surfaces[i + 36], -lines[j + 2 * k + 1]])
-        curves.append(gmsh.model.geo.addSurfaceFilling([cl]))
-        i += 1
-
-for k in range(4):
-    del surfaces[4:len(surfaces):8 - k]
-
-for i in range(0, len(surfaces) // 2, 4):
-    print(i)
-    sl1 = gmsh.model.geo.addSurfaceLoop([surfaces[i], surfaces[i + 1], surfaces[i + 3], surfaces[i + 2]])
-    sl2 = gmsh.model.geo.addSurfaceLoop(
-        [surfaces[i + 16], surfaces[i + 1 + 16], surfaces[i + 3 + 16], surfaces[i + 2 + 16]])
-    if i == 12:
-        sl3 = gmsh.model.geo.addSurfaceLoop([curves[i], curves[i + 1], curves[i + 3], curves[i + 2]])
-        sl4 = gmsh.model.geo.addSurfaceLoop([curves[0], curves[1], curves[3], curves[2]])
-    else:
-        sl3 = gmsh.model.geo.addSurfaceLoop([curves[i], curves[i + 1], curves[i + 3], curves[i + 2]])
-        sl4 = gmsh.model.geo.addSurfaceLoop([curves[i + 4], curves[i + 5], curves[i + 7], curves[i + 6]])
-
-    gmsh.model.geo.addVolume([sl1, sl2, sl3, sl4])
-
+gmsh.model.geo.removeAllDuplicates()
 gmsh.model.geo.synchronize()
-gmsh.model.mesh.generate(2)
+gmsh.model.mesh.generate(3)
 gmsh.write("1.1.msh")
 if '-nopopup' not in sys.argv:
     gmsh.fltk.run()
